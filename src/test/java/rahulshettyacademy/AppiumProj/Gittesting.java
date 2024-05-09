e0f4ccfc290e6b06379bd12ef94e6c515125bdaa
indu22test <indu22test@gmail.com>
2024-05-09 16:54:43 -0400

    second commit

---
+++ src/test/java/rahulshettyacademy/AppiumProj/Gittesting.java
@@ -0,0 +1,12 @@
+package rahulshettyacademy.AppiumProj;
+
+public class Gittesting {
+
+	
+	
+	public void gittesting() {
+		System.out.println("Print jira1");
+		System.out.println("Print jira2");
+		System.out.println("Print jira3");
+	}
+}
